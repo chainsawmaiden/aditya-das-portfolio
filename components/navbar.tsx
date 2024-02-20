49930d9ab2fc3b47bf6797204c5734f7903bcb90
aditya das <55331202+chainsawmaiden@users.noreply.github.com>
2024-02-19 23:53:04 -0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ export default function NavBar({home}: {home?: boolean}) {
     <Link href='/' className="hover:opacity-80 transition">Work</Link>
     <Link href='/play' className="hover:opacity-80 transition">Play</Link>
     <Link href='/about' className="hover:opacity-80 transition">About</Link>
-    <Link href='mailto:aditya.das@yale.edu' target='_blank' className="hover:opacity-80 transition">Email</Link>
+    <span><Link href='mailto:aditya.das@yale.edu' target='_blank' className="hover:opacity-80 transition">Email</Link> / <Link href='https://www.linkedin.com/in/aditya-das-me/' target='_blank' className="hover:opacity-80 transition">Linkedin</Link></span>
   </header>
   )
 }
