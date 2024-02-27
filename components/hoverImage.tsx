4cbe992498b61540e5455e8a2481f3c075031b53
aditya das <55331202+chainsawmaiden@users.noreply.github.com>
2024-02-27 02:32:41 -0500

    Update hoverImage.tsx

--- components/hoverImage.tsx
+++ components/hoverImage.tsx
@@ -5,7 +5,7 @@ export default function HoverImage({children, src}: Readonly<{ children: React.R
   return (
     <span className='font-ppmondwest md:text-xlmw text-mdmw group cursor-help'>
       {children}
-      <Image className='group-hover:opacity-100 absolute md:max-w-96 md:max-h-[75vh] max-h-[30vh] h-auto w-auto md:top-[42vh] top-[35vh] left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 pointer-events-none' src={src} alt='image'/>
+      <Image className='group-hover:opacity-100 absolute w-[75vh] h-[75vh] object-contain top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 pointer-events-none' src={src} alt='image'/>
     </span>
   )
 }
