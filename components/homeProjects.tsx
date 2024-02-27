7381c38a2593f012d265e2126bf24bfabc1bdb80
aditya das <55331202+chainsawmaiden@users.noreply.github.com>
2024-02-27 01:03:17 -0500

    priority

--- components/homeProjects.tsx
+++ components/homeProjects.tsx
@@ -12,7 +12,7 @@ export default function HomeProjects() {
       <TwoColumn>
       {homeProjects.map((homeProject: homeProjectsType, i: number) => (
         <Link href={homeProject.slug} className={`group flex flex-col w-full /lg:w-3/4 mx-auto ${i< 0 && 'col-span-2'}`} key={homeProject.name}>
-          <Img src={homeProject.cover} video/>
+          <Img src={homeProject.cover} video priority/>
           
           <div className='flex justify-between py-1.5 group-hover:opacity-80 transition'>
             <p className='text-left'>{homeProject.name}</p>
--- components/img.tsx
+++ components/img.tsx
@@ -1,9 +1,9 @@
 import Image, { StaticImageData } from 'next/image'
 import React from 'react'
 
-export default function Img({src, square, video, hfill, text, hmax, fit}: {src: StaticImageData, square?: boolean, video?: boolean, hfill?: boolean, text?: boolean, hmax?: boolean, fit?: boolean}) {
+export default function Img({src, square, video, hfill, text, hmax, fit, priority}: {src: StaticImageData, square?: boolean, video?: boolean, hfill?: boolean, text?: boolean, hmax?: boolean, fit?: boolean, priority?: boolean,}) {
 
   return (
-    <Image src={src} alt='Project image' className={`w-full ${fit ? 'object-contain' : 'object-cover'} ${square && 'aspect-square'} ${hmax && 'max-h-screen'} ${hfill ? 'h-full' : 'h-auto'} ${video && 'aspect-video'} ${text && 'max-w-text'}`}/>
+    <Image src={src} alt='Project image' className={`w-full ${fit ? 'object-contain' : 'object-cover'} ${square && 'aspect-square'} ${hmax && 'max-h-screen'} ${hfill ? 'h-full' : 'h-auto'} ${video && 'aspect-video'} ${text && 'max-w-text'}`} priority={priority}/>
   )
 }
