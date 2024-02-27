b735c88f18e4ab96f65d995c8ace13e27852028e
aditya das <55331202+chainsawmaiden@users.noreply.github.com>
2024-02-27 01:28:42 -0500

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <main className="w-full px-4 font-ppneuemontreal text-smnm gap-0 flex flex-col">
         <NavBar home />
-        <h1 className="md:text-xlnm text-mdnm text-left w-5/6 mr-auto pt-18 lg:pb-24 py-[4.5rem]">
+        <h1 className="md:text-xlnm text-mdnm text-left w-5/6 mr-auto pt-12 lg:pb-24 py-[4.5rem]">
           <HoverImage src={me}>Aditya Das</HoverImage> is a multidisciplinary <HoverImage src={danceNation}>designer</HoverImage> and engineer based in <HoverImage src={newHaven}>New Haven, CT</HoverImage>. He is passionate about exploring the intersection of <HoverImage src={sculptor}>art</HoverImage> and <HoverImage src={arm}>technology</HoverImage> to tell stories and <HoverImage src={music}>foster human connection</HoverImage>. He has previously designed for <HoverImage src={jam}>17o1 Records</HoverImage> and Cimu. He also does <HoverImage src={bear}>odd jobs</HoverImage> on the side. He is currently working as a product designer at Yale Law School. ↓
         </h1>
       <HomeProjects />
--- components/homeProjects.tsx
+++ components/homeProjects.tsx
@@ -11,7 +11,7 @@ export default function HomeProjects() {
     <section className='flex flex-col gap-3 text-ppneuemontreal md:text-smnm text-xsnm'>
       <TwoColumn>
       {homeProjects.map((homeProject: homeProjectsType, i: number) => (
-        <Link href={homeProject.slug} className={`group flex flex-col w-full /lg:w-3/4 mx-auto ${i< 0 && 'col-span-2'}`} key={homeProject.name}>
+        <Link href={homeProject.slug} className={`group flex flex-col w-full /lg:w-3/4 mx-auto ${i<= 0 && 'col-span-2'}`} key={homeProject.name}>
           <Img src={homeProject.cover} video priority/>
           
           <div className='flex justify-between py-1.5 group-hover:opacity-80 transition'>
