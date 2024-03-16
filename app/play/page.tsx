a8da41fd66a24d394fadcdcb1790edd30635552f
Aditya Das <ziggydas@gmail.com>
2024-03-15 21:09:38 -0600

    z

---
+++ app/play/page.tsx
@@ -0,0 +1,12 @@
+
+import NavBar from '@/components/navbar'
+import React from 'react'
+
+export default function PopcornPage() {
+  return (
+    <main className="w-full px-4 font-ppneuemontreal text-smnm gap-0 flex flex-col">
+      <NavBar />
+      hiii
+    </main>
+  )
+}
