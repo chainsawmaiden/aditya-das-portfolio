c6d91f133c2c132dbb69bf903ca77d2a2d9f0ae7
Aditya Das <ziggydas@gmail.com>
2024-03-27 16:51:53 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer({article}: {article?: boolean}) {
     <>
     <div className='grow'></div>
     <footer className={`${article && 'px-3'} mt-16 pb-1 w-full sticky top-0 left-0 opacity-50 text-black flex justify-between font-ppneuebit font-bold text-sm uppercase justify-self-end`}>  
-      <Link href='/' className="hover:opacity-80 transition">Aditya Das © 2024<span className="sm:inline hidden"> / Made with love (and NextJS)</span></Link>
+      <Link href='/' className="hover:opacity-80 transition">Aditya Das © 2024<span className="sm:inline hidden"> / Made with love (and Next.js)</span></Link>
       <Link href='mailto:aditya.das@yale.edu' target='_blank' className="hover:opacity-80 transition"><span className="sm:inline hidden">The internet is lonely... </span>talk to me!</Link>
     </footer>
     </>
