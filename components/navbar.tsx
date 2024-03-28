75116c9d6982c12fb997eb19fcf2e9ba4cfb6b15
Aditya Das <ziggydas@gmail.com>
2024-03-28 17:52:26 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,8 +8,8 @@ export default function NavBar({home, article}: {home?: boolean, article?: boole
     <Link href='/play' className="hover:opacity-80 transition">Play</Link>
     <Link href='/about' className="hover:opacity-80 transition">About</Link>
     <span><Link href='mailto:aditya.das@yale.edu' target='_blank' className="hover:opacity-80 transition">Email</Link> / 
-    <Link href='https://www.linkedin.com/in/aditya-das-me/' target='_blank' className="hover:opacity-80 transition">Linkedin</Link> / 
-    <Link href="/Aditya Das' Resume.pdf" target='_blank' rel="noopener noreferrer">CV</Link></span>
+    <Link href='https://www.linkedin.com/in/aditya-das-me/' target='_blank' className="hover:opacity-80 transition"> Linkedin</Link> / 
+    <Link href="/Aditya Das' Resume.pdf" target='_blank' rel="noopener noreferrer"> CV</Link></span>
   </header>
   )
 }
