1568585517e73acaa480e7f016d57ab903b9c95d
Aditya Das <ziggydas@gmail.com>
2024-04-12 10:24:13 -0400

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function NavBar({home, article}: {home?: boolean, article?: boolean}) {
   return (
-  <header className={`w-full  top-0 z-50 ${article ? 'px-4 fixed' : 'sticky'} ${home ? 'pb-0' : 'pb-6'} left-0 text-black opacity-50 py-1 font-ppneuebit font-bold text-sm lg:text-lg uppercase flex justify-between`}>
+  <header className={`w-full  top-0 z-50 ${article ? 'px-4 fixed' : 'sticky'} ${home ? 'pb-0' : 'pb-6'} left-0 text-black opacity-50 py-1 font-ppneuebit font-bold text-sm md:text-lg uppercase flex justify-between`}>
     <Link href='/' className="hover:opacity-80 transition">Work</Link>
     <Link href='/play' className="hover:opacity-80 transition">Misc</Link>
     <Link href='/about' className="hover:opacity-80 transition">About</Link>
