f02015a407cc7967de20846858e9acf0e89cd242
Aditya Das <ziggydas@gmail.com>
2024-04-25 13:49:05 -0400

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export default function NavBar({home, article}: {home?: boolean, article?: boolean}) {
   return (
-  <header className={`w-full  top-0 z-50 ${article ? 'px-4 fixed' : 'sticky'} ${home ? 'pb-0' : 'pb-6'} left-0 text-black opacity-50 py-1 font-ppneuebit font-bold text-sm md:text-lg uppercase flex justify-between`}>
-    <Link href='/' className="hover:opacity-80 transition">Work</Link>
-    <Link href='/play' className="hover:opacity-80 transition">Misc</Link>
+  <header className={`w-full  top-0 z-50 ${article ? 'px-4 fixed' : 'sticky'} ${home ? 'pb-0' : 'mb-6'} left-0 //bg-white mix-blend-difference text-gray-200 //opacity-50 py-1 font-ppneuebit font-bold text-sm md:text-lg uppercase flex justify-between`}>
+    <Link href='/' className="hover:opacity-80 transition ">Work</Link>
+    <Link href='/misc' className="hover:opacity-80 transition">Misc</Link>
     <Link href='/about' className="hover:opacity-80 transition">About</Link>
     <span><Link href='mailto:aditya.das@yale.edu' target='_blank' className="hover:opacity-80 transition">Email</Link> / 
     <Link href='https://www.linkedin.com/in/aditya-das-me/' target='_blank' className="hover:opacity-80 transition"> Linkedin</Link> / 
